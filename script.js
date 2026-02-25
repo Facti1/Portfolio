7322ba9049c9b1905a32608a6ec527ff365856d2
Francis Abdul <105426311+Facti1@users.noreply.github.com>
2026-02-25 14:37:05 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (let tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+
+    for (let tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+    
+}
+
+// menu 
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+
+// contact 
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyszt3tiH36mKQIfW3d7M7CvriZ-UYZllu37WVL-o7zm86UENKG9rjsx_Th4e-eDVTu/exec'
+const form = document.forms['submit-to-google']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(() => {
+            msg.innerHTML = "Message sent"
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
